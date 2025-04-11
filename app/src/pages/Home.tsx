96ede53df220e88e413f1da844cb40993e2ee03f
Katie McGinley <katie@mcgnly.com>
2025-04-11 12:09:01 +0200

    add eslint and prettierrc

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 export function HomePage() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <div className="home-page">
       <h2>Home Page</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/pages/Home.tsx</code> and save to test HMR
         </p>
       </div>
     </div>
-  );
+  )
 }
