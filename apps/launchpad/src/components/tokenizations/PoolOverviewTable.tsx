d2bdcd7d55de980755b020de45b914a933d18ec1
Katty Barroso <kattybarroso1321@gmail.com>
2025-06-25 17:31:39 +0200

    feat: add table

--- apps/launchpad/src/components/tokenizations/PoolOverviewTable.tsx
+++ apps/launchpad/src/components/tokenizations/PoolOverviewTable.tsx
@@ -1,23 +1,63 @@
 import { useAllPoolDetails, usePool, usePools } from '@centrifuge/shared'
-import { Table } from '@chakra-ui/react'
+import { Box, Heading, Image, Stack, Table } from '@chakra-ui/react'
+import DataTable, { ColumnDefinition } from './DataTable'
+import { ipfsToHttp } from '@centrifuge/shared/src/utils/formatting'
 
-const columns = ['Pool', 'Investments', 'Redemptions', 'Token', 'Networks', 'APY', 'NAV (USDC)', 'TokenPrice']
+type Row = {
+  id: string
+  name: string
+  icon: { uri: string } | undefined
+  shareClasses: { token: string; apy: string; tokenPrice: string }[]
+}
 
-// TODO: table MAYBE should be reusable, is the only table of this type in the app
+const columns: ColumnDefinition<Row>[] = [
+  {
+    header: 'Pool',
+    accessor: 'name',
+    render: ({ icon, name }: Row) => {
+      return (
+        <Box>
+          <Image src={ipfsToHttp(icon?.uri ?? '')} alt={name} />
+          <Heading>{name}</Heading>
+        </Box>
+      )
+    },
+  },
+]
+
 export const PoolOverviewTable = () => {
   const { data: pools } = useAllPoolDetails()
+
+  const data =
+    pools?.map((pool) => {
+      const shareClassDetails = Object.values(pool.shareClasses)
+      return {
+        id: pool.id,
+        name: pool.metadata?.pool.name,
+        icon: pool.metadata?.pool.icon,
+        shareClasses: shareClassDetails.map((sc) => ({
+          token: sc.details.symbol,
+          apy: pool.metadata?.shareClasses[0]?.apy ?? '-',
+          tokenPrice: sc.details.navPerShare,
+        })),
+      }
+    }) ?? []
+
+  const flattenedData = data.flatMap((d) =>
+    d.shareClasses.map((sc) => ({
+      id: `${d.id}-${sc.token}`,
+      poolName: d.name,
+      iconUri: d.icon?.uri,
+      token: sc.token,
+      apy: sc.apy,
+      tokenPrice: sc.tokenPrice,
+    }))
+  )
+
   return (
-    <>
-      <Table.Root size="sm" variant="outline" striped borderRadius="md" borderColor="1px solid border-primary">
-        <Table.Header backgroundColor="white">
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeader key={column}>{column}</Table.ColumnHeader>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body></Table.Body>
-      </Table.Root>
-    </>
+    <Stack>
+      <Heading>Pool overview</Heading>
+      <DataTable columns={columns} data={flattenedData ?? []} />
+    </Stack>
   )
 }
