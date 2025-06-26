cc9c665e716e736d5e74ab786e913e545b5292e8
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-06-26 13:14:35 +0200

    feat(launchpad): update pool overview table

--- apps/launchpad/src/components/tokenizations/PoolOverviewTable.tsx
+++ apps/launchpad/src/components/tokenizations/PoolOverviewTable.tsx
@@ -1,15 +1,19 @@
 import { useAllPoolDetails } from '@centrifuge/shared'
-import { Box, Heading, Image, Stack } from '@chakra-ui/react'
+import { Flex, Heading, Image, Stack } from '@chakra-ui/react'
 import DataTable, { ColumnDefinition } from './DataTable'
 import { ipfsToHttp } from '@centrifuge/shared/src/utils/formatting'
+import { Balance } from '@centrifuge/sdk'
+import { BalanceDisplay, NetworkIcon } from '@centrifuge/ui'
 
 type Row = {
   id: string
   poolName: string
   iconUri: string
   token: string
+  networks: number
   apy: string
-  tokenPrice: string
+  nav: Balance
+  tokenPrice: Balance
 }
 
 const columns: ColumnDefinition<Row>[] = [
@@ -18,12 +22,35 @@ const columns: ColumnDefinition<Row>[] = [
     accessor: 'name',
     render: ({ poolName, iconUri }: Row) => {
       return (
-        <Box>
-          <Image src={ipfsToHttp(iconUri ?? '')} alt={poolName} />
-          <Heading>{poolName}</Heading>
-        </Box>
+        <Flex alignItems="center">
+          <Image src={ipfsToHttp(iconUri ?? '')} alt={poolName} height="36px" />
+          <Heading size="sm">{poolName}</Heading>
+        </Flex>
       )
     },
+  },
+  {
+    header: 'Token',
+    accessor: 'token',
+  },
+  {
+    header: 'Networks',
+    accessor: 'networks',
+    render: () => <NetworkIcon network="ethereum" />,
+  },
+  {
+    header: 'APY',
+    accessor: 'apy',
+  },
+  {
+    header: 'Nav',
+    accessor: 'nav',
+    render: ({ nav }) => <BalanceDisplay balance={nav} />,
+  },
+  {
+    header: 'Token Price',
+    accessor: 'tokenPrice',
+    render: ({ tokenPrice }) => <BalanceDisplay balance={tokenPrice} />,
   },
 ]
 
@@ -34,24 +61,30 @@ export const PoolOverviewTable = () => {
     pools?.map((pool) => {
       const shareClassDetails = Object.values(pool.shareClasses)
       return {
+        // currency: pool.currency,
         id: pool.id,
-        name: pool.metadata?.pool.name,
+        name: pool.metadata?.pool.name ?? 'RWA Portfolio',
         icon: pool.metadata?.pool.icon,
         shareClasses: shareClassDetails.map((sc) => ({
           token: sc.details.symbol,
-          apy: pool.metadata?.shareClasses[0]?.apy ?? '-',
+          apy: pool.metadata?.shareClasses?.apy ?? '-',
           tokenPrice: sc.details.navPerShare,
+          totalIssuance: sc.details.totalIssuance,
+          networks: sc.shareClass.pool.chainId,
         })),
       }
     }) ?? []
 
   const flattenedData = data.flatMap((d) =>
     d.shareClasses.map((sc) => ({
+      // currency: d.currency,
       id: `${d.id}-${sc.token}`,
       poolName: d.name,
       iconUri: d.icon?.uri,
       token: sc.token,
+      networks: sc.networks,
       apy: sc.apy,
+      nav: sc.totalIssuance,
       tokenPrice: sc.tokenPrice,
     }))
   )
