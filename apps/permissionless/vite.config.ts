8445cdd741ae060590511c9f3b1166d5d432b1af
Katty Barroso <kattybarroso1321@gmail.com>
2025-07-02 15:27:33 +0200

    fix: hooks

--- apps/permissionless/vite.config.ts
+++ apps/permissionless/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import { cloudflare } from '@cloudflare/vite-plugin'
 import path from 'path'
 
 export default defineConfig(() => {
