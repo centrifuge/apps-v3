fb409118d6bb9a9c1d9b19e1ed289b8e8e63f5fc
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-07-03 16:15:02 +0200

    fix: success panel transaction state view

--- apps/permissionless/src/components/InvestRedeemSection/components/SuccessPanel.tsx
+++ apps/permissionless/src/components/InvestRedeemSection/components/SuccessPanel.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Flex, Heading, Icon, Text } from '@chakra-ui/react'
-import { IoMdCheckmarkCircleOutline } from 'react-icons/io'
+import { Box, Button, Flex, Heading, Icon, Text, useToken } from '@chakra-ui/react'
+import { IoIosCloseCircleOutline, IoMdCheckmarkCircleOutline, IoMdTimer } from 'react-icons/io'
 import { useFormContext } from '@centrifuge/forms'
 import { InvestAction, RedeemAction, type InvestActionType, type RedeemActionType } from './defaults'
-import type { Dispatch, SetStateAction } from 'react'
+import { useEffect, useRef, useState, type Dispatch, type SetStateAction } from 'react'
+import { useTransactions } from '@components/Transactions/TransactionProvider'
 
 interface InvestActionProps {
   currencies: { investCurrency: string; receiveCurrency: string }
@@ -22,8 +23,71 @@ type SuccessPanelProps = InvestActionProps | RedeemActionProps
 // to ensure TypeScript can correctly infer the type based on the isInvesting property.
 export function SuccessPanel(props: SuccessPanelProps) {
   const { getValues } = useFormContext()
+  const [txDescription, setTxDescription] = useState('Handling transaction')
+  const [isTxSuccessful, setIsTxSuccessful] = useState(false)
+  const [isTxFailed, setIsTxFailed] = useState(false)
+  const [txHash, setTxHash] = useState<string | null>(
+    '0x84568c28813859bcc1bbc50725b84319ec42329c7d099712c0c45155db0373ae'
+  )
+  const { transactions } = useTransactions()
+  const [successColor, errorColor] = useToken('colors', ['success', 'error'])
+
+  const activeTransactionIds = useRef<Record<string, string>>({})
+
+  useEffect(() => {
+    transactions.forEach((tx) => {
+      if (['creating', 'unconfirmed', 'pending'].includes(tx.status) && !tx.dismissed) {
+        if (!activeTransactionIds.current[tx.id]) {
+          if (tx.status === 'creating') {
+            setTxDescription('Creating transaction')
+          } else if (tx.status === 'unconfirmed') {
+            setTxDescription('Signing transaction')
+          } else if (tx.status === 'pending') {
+            setTxDescription('Transaction pending')
+          } else {
+            setTxDescription('Handling transaction')
+          }
+        }
+
+        activeTransactionIds.current[tx.id] = tx.id
+      } else if (['succeeded', 'failed'].includes(tx.status) && activeTransactionIds.current[tx.id]) {
+        if (tx.status === 'failed' && !!tx.failedReason?.length) {
+          setTxDescription(tx.failedReason)
+          setIsTxFailed(true)
+        } else if (tx.status === 'failed') {
+          setTxDescription('Transaction failed')
+          setIsTxFailed(true)
+        } else if (tx.status === 'succeeded' && tx.title === 'Invest') {
+          setTxDescription('Invest successful')
+          setIsTxSuccessful(true)
+          setTxHash(tx.result?.transactionHash ?? null)
+        } else if (tx.status === 'succeeded' && tx.title === 'Redeem') {
+          setTxDescription('Redeem successful')
+          setIsTxSuccessful(true)
+          setTxHash(tx.result?.transactionHash ?? null)
+        } else {
+          setTxDescription('Transaction status unknown')
+        }
+
+        delete activeTransactionIds.current[tx.id]
+      } else if (tx.dismissed && activeTransactionIds.current[tx.id]) {
+        delete activeTransactionIds.current[tx.id]
+      }
+    })
+
+    const currentTxIds = new Set(transactions.map((t) => t.id))
+    for (const txId in activeTransactionIds.current) {
+      if (!currentTxIds.has(txId)) {
+        delete activeTransactionIds.current[txId]
+      }
+    }
+  }, [transactions])
+
   const isInvesting = props.isInvesting
   const buttonText = isInvesting ? 'Invest more' : 'Redeem more'
+  const txHeaderColor = isTxSuccessful ? successColor : isTxFailed ? errorColor : 'inherit'
+  const isButtonDisabled = !isTxSuccessful && !isTxFailed
+  console.log('SuccessPanel', { isTxSuccessful, isTxFailed, isButtonDisabled, transactions })
 
   return (
     <Box height="100%">
@@ -35,27 +99,50 @@ export function SuccessPanel(props: SuccessPanelProps) {
         width="100%"
         height="100%"
       >
-        <Box width="100%">
+        <Box width="100%" overflow="hidden">
           <Flex alignItems="center" gap={2} justifyContent="space-between">
-            <Heading>{isInvesting ? 'Investment' : 'Redemption'} successful</Heading>
+            <Heading color={txHeaderColor}>{txDescription}</Heading>
             <Icon size="xl">
-              <IoMdCheckmarkCircleOutline />
+              {isTxSuccessful ? (
+                <IoMdCheckmarkCircleOutline color={txHeaderColor} />
+              ) : isTxFailed ? (
+                <IoIosCloseCircleOutline color={txHeaderColor} />
+              ) : (
+                <IoMdTimer color="gray.400" />
+              )}
             </Icon>
           </Flex>
-          <Flex alignItems="center" gap={2} justifyContent="space-between" mt={6} width="100%">
-            <Box>
-              <Text fontWeight={500}>You {isInvesting ? 'invested' : 'redeemed'}</Text>
-              <Heading fontSize="lg">{getValues('amount').toString()}</Heading>
-            </Box>
-            <Text alignSelf="flex-end">{props.currencies.investCurrency}</Text>
-          </Flex>
-          <Flex alignItems="center" gap={2} justifyContent="space-between" mt={6}>
-            <Box>
-              <Text fontWeight={500}>{isInvesting ? 'Token amount' : 'You receive'}</Text>
-              <Heading fontSize="lg">{getValues('amountToReceive').toString()}</Heading>
-            </Box>
-            <Text alignSelf="flex-end">{props.currencies.receiveCurrency}</Text>
-          </Flex>
+          <Box opacity={isTxSuccessful ? 1 : 0.5}>
+            <Flex alignItems="center" gap={2} justifyContent="space-between" mt={6} width="100%">
+              <Box>
+                <Text fontWeight={500}>You {isInvesting ? 'invested' : 'redeemed'}</Text>
+                <Heading fontSize="lg">{getValues('amount').toString()}</Heading>
+              </Box>
+              <Text alignSelf="flex-end">{props.currencies.investCurrency}</Text>
+            </Flex>
+            <Flex alignItems="center" gap={2} justifyContent="space-between" mt={6}>
+              <Box>
+                <Text fontWeight={500}>{isInvesting ? 'Token amount' : 'You receive'}</Text>
+                <Heading fontSize="lg">{getValues('amountToReceive').toString()}</Heading>
+              </Box>
+              <Text alignSelf="flex-end">{props.currencies.receiveCurrency}</Text>
+            </Flex>
+          </Box>
+          {txHash && (
+            <Flex alignItems="center" gap={2} justifyContent="space-between" mt={6} opacity={isTxSuccessful ? 1 : 0.5}>
+              <Box>
+                <Text fontWeight={500}>Transaction hash</Text>
+                <Text fontSize="xs" wordBreak="break-word">
+                  {txHash}
+                </Text>
+              </Box>
+              <Button asChild variant="subtle" size="xs">
+                <a target="_blank" href={`https://etherscan.io/tx/${txHash}`}>
+                  View on Etherscan
+                </a>
+              </Button>
+            </Flex>
+          )}
         </Box>
         <Button
           colorPalette="yellow"
@@ -70,6 +157,7 @@ export function SuccessPanel(props: SuccessPanelProps) {
           }}
           width="100%"
           mt={4}
+          disabled={isButtonDisabled}
         >
           {buttonText}
         </Button>
