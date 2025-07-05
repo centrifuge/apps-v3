b9d4c33dc7d6cf975d0391950e966ef04f9d9831
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-07-05 11:41:18 +0200

    fix(beta.app): infinite loop error crashing app on re-render

--- apps/permissionless/src/App.tsx
+++ apps/permissionless/src/App.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@chakra-ui/react'
+import { useCentrifugeSigner } from '@centrifuge/shared'
 import { Header } from './components/Header'
 import { PoolPage } from './components/PoolPage'
 import { LandingPageSkeleton } from './components/Skeletons/LandingPageSkeleton'
@@ -6,6 +7,15 @@ import { useSelectedPoolContext } from './contexts/useSelectedPoolContext'
 
 export function App() {
   const { isLoading } = useSelectedPoolContext()
+  /**
+   * Automatically sets the signer on the Centrifuge SDK instance based on wallet connection status.
+   * This hook should be called once in the app, ideally at the top level.
+   * It will set the signer when the wallet is connected and clear it when disconnected.
+   * This is important to ensure the SDK uses the correct signer for transactions.
+   * If we need to use the signer in other parts of the app, we can access
+   * it through the Centrifuge SDK instance obtained from `useCentrifuge()
+   */
+  useCentrifugeSigner()
 
   return (
     <Box bg="bg-secondary" w="100%" minH="100vh">
--- apps/permissionless/src/Root.tsx
+++ apps/permissionless/src/Root.tsx
@@ -1,13 +1,51 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { App } from './App'
+import { WalletProvider } from '@centrifuge/wallet'
+import { SelectedPoolProvider } from './contexts/useSelectedPoolContext'
+import Centrifuge from '@centrifuge/sdk'
+import { CentrifugeProvider } from '@centrifuge/shared'
+import { TransactionProvider } from './components/Transactions/TransactionProvider'
+import { ChakraCentrifugeProvider, ChakraCentrifugeProviderProps } from '@centrifuge/ui'
+import { useMemo } from 'react'
+
+const config = {
+  themeKey: 'light' as ChakraCentrifugeProviderProps['themeKey'],
+}
 
 const queryClient = new QueryClient()
 
 export function Root() {
+  /**
+   * Initialize Centrifuge SDK with any necessary config.
+   * We need to ensure it is created only once, so we must use useMemo.
+   * If we don't use useMemo, it can create a new instance on every render,
+   * which can lead to an infinite loop of new instances querying sdk pools and crashing the app.
+   */
+  const centrifuge = useMemo(
+    () =>
+      new Centrifuge({
+        environment: import.meta.env.VITE_CENTRIFUGE_ENV,
+        rpcUrls: { 11155111: `https://eth-sepolia.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_KEY}` },
+      }),
+    []
+  )
+
+  const networks = useMemo(() => centrifuge.chains.map((cid) => centrifuge.getChainConfig(cid)), [centrifuge])
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <CentrifugeProvider client={centrifuge}>
+          <WalletProvider projectId={import.meta.env.VITE_REOWN_APP_ID!} networks={networks}>
+            <ChakraCentrifugeProvider themeKey={config.themeKey}>
+              <TransactionProvider>
+                <SelectedPoolProvider>
+                  <App />
+                </SelectedPoolProvider>
+              </TransactionProvider>
+            </ChakraCentrifugeProvider>
+          </WalletProvider>
+        </CentrifugeProvider>
       </QueryClientProvider>
     </>
   )
