b4ac4dd03b4ef37a89528037bbac0385d1ce8b7d
Gregory <wojciech@k-f.co>
2025-07-08 08:56:15 +0200

    remove deferred value from exeperiment

--- packages/shared/src/hooks/useAddress.ts
+++ packages/shared/src/hooks/useAddress.ts
@@ -1,20 +1,12 @@
 import { useAccount } from 'wagmi'
-import { useDeferredValue } from 'react'
 import { useDebugFlags } from '../components/DebugFlags'
 
 const useAddress = () => {
   const { address: debugAddress } = useDebugFlags()
-  const deferredDebugAddress = useDeferredValue(debugAddress)
   const { address, isConnected, chainId } = useAccount()
 
-  if (deferredDebugAddress) {
-    return {
-      address: deferredDebugAddress as `0x${string}`,
-    }
-  }
-
   return {
-    address,
+    address: (debugAddress as `0x${string}`) || address,
     isConnected,
     chainId,
   }
