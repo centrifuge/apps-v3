7cf177ecf56988fd14847f9ef183dbde1720e242
Katty Barroso <kattybarroso1321@gmail.com>
2025-07-15 14:53:55 +0200

    Convert launchpad into SSA

--- apps/launchpad/src/centrifuge.ts
+++ apps/launchpad/src/centrifuge.ts
@@ -3,7 +3,6 @@ import Centrifuge from '@centrifuge/sdk'
 export const centrifuge = new Centrifuge({
   environment: import.meta.env.VITE_CENTRIFUGE_ENV,
   rpcUrls: { 11155111: `https://eth-sepolia.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_KEY}` },
-  indexerUrl: 'https://api.centrifuge.io',
 })
 
 export const networks = centrifuge.chains.map((cid) => centrifuge.getChainConfig(cid))
--- apps/launchpad/vite.config.ts
+++ apps/launchpad/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import { reactRouter } from '@react-router/dev/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
@@ -7,7 +7,7 @@ export default defineConfig(() => {
   const envDir = path.resolve(__dirname, '../../.env-config')
   return {
     envDir,
-    plugins: [reactRouter(), tsconfigPaths()],
+    plugins: [tsconfigPaths()],
     server: {
       port: 3004,
     },
