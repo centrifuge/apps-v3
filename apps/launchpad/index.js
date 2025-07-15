2ed6365cc8e0f9595331ffc9e6256ff7984eda64
Katty Barroso <kattybarroso1321@gmail.com>
2025-07-15 11:48:20 +0200

    Add hono

--- apps/launchpad/index.js
+++ apps/launchpad/index.js
@@ -1,7 +1,12 @@
-// We are building a static site using wokers
-// more info https://developers.cloudflare.com/workers/static-assets/
-export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request)
-  },
-}
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/cloudflare-workers'
+
+const app = new Hono()
+
+app.use('/assets/*', serveStatic({ root: './dist' }))
+
+// This rule is the catch-all. For any other path, it serves the main index.html file.
+// This allows client-side React Router to work.
+app.get('*', serveStatic({ path: './dist/index.html' }))
+
+export default app
