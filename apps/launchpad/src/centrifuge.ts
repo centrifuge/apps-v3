af62b88f03cb8ce70ea93d22b2491a11b7598fd5
Katty Barroso <kattybarroso1321@gmail.com>
2025-07-16 14:27:53 +0200

    fix: add indexer url

--- apps/launchpad/src/centrifuge.ts
+++ apps/launchpad/src/centrifuge.ts
@@ -3,6 +3,7 @@ import Centrifuge from '@centrifuge/sdk'
 export const centrifuge = new Centrifuge({
   environment: 'testnet',
   rpcUrls: { 11155111: `https://eth-sepolia.g.alchemy.com/v2/KNR-1LZhNqWOxZS2AN8AFeaiESBV10qZ` },
+  indexerUrl: 'https://api-v3-hitz.marble.live/graphql',
 })
 
 export const networks = centrifuge.chains.map((cid) => centrifuge.getChainConfig(cid))
