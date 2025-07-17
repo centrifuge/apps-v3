a4182afbd19ecb01f9b7812793aaaa08c31ef0c0
Katty Barroso <kattybarroso1321@gmail.com>
2025-07-17 14:00:59 +0200

    fix: variable env

--- apps/derwa/src/Root.tsx
+++ apps/derwa/src/Root.tsx
@@ -26,8 +26,8 @@ export function Root() {
   const centrifuge = useMemo(
     () =>
       new Centrifuge({
-        environment: import.meta.env.VITE_ENVIRONMENT,
-        rpcUrls: { 11155111: `https://eth-sepolia.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}` },
+        environment: import.meta.env.VITE_CENTRIFUGE_ENV,
+        indexerUrl: import.meta.env.VITE_INDEXER_URL,
       }),
     []
   )
