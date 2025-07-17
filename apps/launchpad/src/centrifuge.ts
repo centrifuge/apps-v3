aec4e879c3617c4dc036b23eccc9c95c2c8ba5fe
Katty Barroso <kattybarroso1321@gmail.com>
2025-07-17 12:53:53 +0200

    deploy mainnet

--- apps/launchpad/src/centrifuge.ts
+++ apps/launchpad/src/centrifuge.ts
@@ -1,6 +1,6 @@
 import Centrifuge from '@centrifuge/sdk'
 
-const centrifuge = new Centrifuge({
+export const centrifuge = new Centrifuge({
   environment: import.meta.env.VITE_ENVIRONMENT,
   rpcUrls: { 11155111: `https://eth-sepolia.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}` },
 })
