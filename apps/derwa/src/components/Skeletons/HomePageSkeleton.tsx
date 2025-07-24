7aad3e10823a9a36dedc63d7364ae336e89f9721
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-07-24 15:06:16 +0200

    fix(derwa): home page skeleton keys

--- apps/derwa/src/components/Skeletons/HomePageSkeleton.tsx
+++ apps/derwa/src/components/Skeletons/HomePageSkeleton.tsx
@@ -5,8 +5,8 @@ export function HomePageSkeleton() {
 
   return (
     <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }} gap="6">
-      {cards.map(() => (
-        <Skeleton height="292px" width="315px" borderRadius="md" />
+      {cards.map((_, index) => (
+        <Skeleton key={index} height="292px" borderRadius="md" />
       ))}
     </Grid>
   )
