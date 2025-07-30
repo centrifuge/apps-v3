10410d7683285069d79dde597543b448a2394188
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-07-30 17:55:13 +0200

    feat(derwa): update app background

--- apps/derwa/src/layouts/MainLayout.tsx
+++ apps/derwa/src/layouts/MainLayout.tsx
@@ -3,21 +3,24 @@ import { Outlet } from 'react-router-dom'
 import { Box, Separator } from '@chakra-ui/react'
 import { ErrorBoundary, LogoCentrifugeText } from '@centrifuge/ui'
 import { WalletButton } from '@centrifuge/wallet'
+import mainBg from '../assets/main-bg.png'
 
 const MainLayout = memo(() => {
   return (
-    <Box bg="bg-secondary" w="100%" minH="100vh">
-      <Box maxW={{ base: '95vw', xl: '80vw' }} mx="auto" px={{ base: 4, md: 8 }} py={{ base: 4, md: 8 }}>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Box width={40} mb={8}>
-            <LogoCentrifugeText fill="text-primary" />
+    <Box bg="white">
+      <Box minH="100vh" bgImage={`url(${mainBg})`} bgRepeat="no-repeat" bgSize="cover" backgroundPosition="center">
+        <Box maxW={{ base: '95vw', xl: '80vw' }} mx="auto" px={{ base: 4, md: 8 }} py={{ base: 4, md: 8 }}>
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Box width={40} mb={8}>
+              <LogoCentrifugeText fill="text-primary" />
+            </Box>
+            <WalletButton />
           </Box>
-          <WalletButton />
+          <Separator mb={4} />
+          <ErrorBoundary>
+            <Outlet />
+          </ErrorBoundary>
         </Box>
-        <Separator mb={4} />
-        <ErrorBoundary>
-          <Outlet />
-        </ErrorBoundary>
       </Box>
     </Box>
   )
