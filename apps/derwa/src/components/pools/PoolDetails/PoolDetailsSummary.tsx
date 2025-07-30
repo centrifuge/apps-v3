ca4c18999834a70b2981174764945f8e132ef454
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2025-07-30 22:19:50 +0200

    fix(derwa): pool summary card

--- apps/derwa/src/components/pools/PoolDetails/PoolDetailsSummary.tsx
+++ apps/derwa/src/components/pools/PoolDetails/PoolDetailsSummary.tsx
@@ -1,5 +1,5 @@
 import { formatBigintToString } from '@centrifuge/shared'
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Grid, GridItem, Text } from '@chakra-ui/react'
 import { usePoolsContext } from '@contexts/usePoolsContext'
 
 export function PoolDetailsSummary() {
@@ -27,26 +27,30 @@ export function PoolDetailsSummary() {
 
   return (
     <Box bg="bg-primary" padding={6} borderRadius={10} border="1px solid" borderColor="border-primary" shadow="xs">
-      <Flex
-        justifyContent={{ base: 'center', md: 'flex-start' }}
-        alignItems="center"
-        flexDirection={{ base: 'column', md: 'row' }}
-      >
+      <Grid templateColumns={{ base: 'repeat(3, 1fr)', lg: '7fr 6fr 4fr' }} gap={4}>
         {items.map((item, index) => (
-          <Box
-            padding={{ base: '1rem 0 0 0', md: '0 2.5rem 0 1.5rem' }}
-            borderLeft={index > 0 ? { base: 'none', md: '1px solid #E7E7E7' } : 'none'}
-            key={item.label}
-          >
-            <Text fontSize="12px" color="black" width="auto" textAlign={{ base: 'center', md: 'left' }}>
-              {item.label}
-            </Text>
-            <Text fontSize="xl" fontWeight="bold" textAlign={{ base: 'center', md: 'left' }}>
-              {item.value}
-            </Text>
-          </Box>
+          <GridItem minW={0} overflow="hidden" position="relative">
+            <Box
+              padding={{ base: '1rem 0 0 0', md: '0 1rem' }}
+              borderLeft={index > 0 ? { base: 'none', md: '1px solid #E7E7E7' } : 'none'}
+              textAlign={{ base: 'center', md: 'left' }}
+            >
+              <Text fontSize="12px" color="black" width="auto" textAlign={{ base: 'center', md: 'left' }}>
+                {item.label}
+              </Text>
+              <Text
+                fontSize="clamp(0.5rem, 1rem, 1rem)"
+                whiteSpace="nowrap"
+                overflow="hidden"
+                textOverflow="ellipsis"
+                fontWeight={500}
+              >
+                {item.value}
+              </Text>
+            </Box>
+          </GridItem>
         ))}
-      </Flex>
+      </Grid>
     </Box>
   )
 }
